d7bb95318b7bb8d823e0ecb5a2f28588cd14aa60
Aswin_Wibowo <aswin05.aw@gmail.com>
2023-03-11 23:01:15 +0700

    SPRINT 4 START HERE

---
+++ client/src/App.js
@@ -0,0 +1,144 @@
+import "./App.css";
+import REST_API from "./support/services/RESTApiService";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landingPage";
+import NavigationBar from "./components/navbar";
+import Profile from "./pages/profile";
+import { useEffect, useState } from "react";
+import Login from "./pages/login";
+import UpdatePassword from "./pages/newpass";
+import Register from "./pages/register";
+import Activation from "./pages/activation";
+import ForgotPass from "./pages/forgotpass";
+import Dashboard from "./pages/dashboard";
+import SalesReport from "./components/salesReport";
+import ProductCategory from "./pages/product_category";
+import Checkout from "./pages/checkout";
+import Cart from "./pages/cart";
+import BranchAdminRegister from "./components/branchAdminRegister";
+import Transaction from "./pages/transaction";
+import LoginAdmin from "./pages/loginAdmin";
+import StockHistory from "./components/stockHistory";
+import { toast } from "react-hot-toast";
+
+function App() {
+	const [disable, setdisable] = useState();
+	const [profile, setprofile] = useState({
+		uid: null,
+		id: null,
+		name: null,
+		birthdate: null,
+		gender: null,
+		email: null,
+		phone_number: null,
+		profile_picture: null,
+		address: null,
+	});
+	const getProfile = async () => {
+		try {
+			const { data } = await REST_API({
+				url: "/user/profile",
+				method: "GET",
+			});
+			setprofile({
+				...profile,
+				uid: data.data.uid,
+				id: data.data.id,
+				name: data.data.name,
+				birthdate: data.data.birthdate,
+				gender: data.data.gender,
+				email: data.data.email,
+				phone_number: data.data.phone_number,
+				profile_picture: data.data.img,
+				address: data.data.user_addresses,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	let onLogin = async (email, password) => {
+		try {
+			setdisable(true);
+			const { data } = await REST_API({
+				url: "/user/login",
+				method: "POST",
+				data: {
+					email: email,
+					password: password,
+				},
+			});
+			await localStorage.setItem("token", `${data.data.token}`);
+			toast.success(data.message);
+			email = "";
+			password = "";
+			getProfile();
+				setTimeout(() => {
+					window.location.href="http://localhost:3000/home"
+				}, 3000);
+				
+		} catch (error) {
+			toast.error(error.response.data.message);
+			console.log(error);
+		} finally {
+			setdisable(false);
+		}
+	};
+
+	useEffect(() => {
+		getProfile();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	let onLogout = () => {
+		localStorage.removeItem("token");
+		setprofile({ ...profile, name: "" });
+	};
+
+	return (
+		<>
+			<div className="relative">
+				<Routes>
+					{/* User */}
+					<Route
+						path="/"
+						element={<NavigationBar state={{ profile }} Func={{ onLogout }} />}
+					>
+						<Route path="home" element={<LandingPage />} />
+						<Route
+							path="profile"
+							element={
+								<Profile
+									func={{ getProfile }}
+									state={{ profile, setprofile }}
+								/>
+							}
+						/>
+						<Route path="category/:product" element={<ProductCategory />} />
+						<Route path="checkout" element={<Checkout />} />
+						<Route path="cart" element={<Cart />} />
+						<Route path="transaction" element={<Transaction />} />
+					</Route>
+					<Route path="/login" element={<Login MyFunc={{ onLogin }} isDisable={{ disable }} />} />
+					<Route path="/updatePassword/:uid" element={<UpdatePassword />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/activation/:uid" element={<Activation />} />
+					<Route path="/forgotpassword" element={<ForgotPass />} />
+					{/* Admin */}
+					<Route path="/admin" element={<Dashboard />}>
+						{/* <Route path="/admin" element={<Overview />} /> */}
+						<Route path="sales-report" element={<SalesReport />} />
+						<Route
+							path="branch-admin-register"
+							element={<BranchAdminRegister />}
+						/>
+						<Route path="stock-history" element={<StockHistory />} />
+					</Route>
+					<Route path="/loginAdmin" element={<LoginAdmin />} />
+				</Routes>
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ server/controller/index.js
@@ -0,0 +1,13 @@
+const productController = require("./productController");
+const userController = require("./userController");
+const adminController = require("./adminController");
+const cartController = require("./cartController");
+const transactionController = require("./transactionController");
+
+module.exports = {
+	productController,
+	userController,
+	adminController,
+	cartController,
+	transactionController,
+};
